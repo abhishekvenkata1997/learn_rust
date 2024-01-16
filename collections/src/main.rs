1a49f555d552226485fcfdef98262f06ba8107cd
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2024-01-16 03:18:59 -0500

    Understanding vectors, hashmap and strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,4 +1,5 @@
-
+use unicode_segmentation::UnicodeSegmentation;
+use std::collections::HashMap;  
 
 fn main() {
     let a = [1, 2, 3];
@@ -45,10 +46,83 @@ fn main() {
         SpreadsheetCell::Int(3),
         SpreadsheetCell::Text(String::from("Second element")),
         SpreadsheetCell::Float(14.32)
-    ]
+    ];
 
     match &row[1] {
         SpreadsheetCell::Int(i) => println!("{}",i),
         _ => println!("Not an integer")
     };
-}
+
+
+    //Lower programming languages need to deal with complexity of a string
+    //Collection of UTF-8 encoded bytes
+
+    let s1 = String::new();
+    let s2 = "intial contents"; //string literals(slices)
+    let s3 = s2.to_string(); //convert slice to string
+    let s4 = String::from("initial contents");
+
+    let mut s = String::from("Hello ");
+    s.push_str("World");
+    s.push('!');
+    println!("{}",s);
+    let s5 = String::from("foo");
+    let s6 = String::from(" bar");
+    let s7 = s5 + &s6;
+    let s8 = format!("{} {}",s7,s6);
+    println!("{}",s8);
+    println!("{}",s7);
+
+    let hello: String = String::from("Hello");
+    //let c: char  = hello[0];
+    //println!("{}",c);
+
+    for b in hello.bytes() {
+        println!("{}",b);
+    }
+
+    for c in hello.chars() {
+        println!("{}",c);
+    }
+
+    for g in hello.graphemes(true) {
+        println!("{}",g);
+    }
+
+    //hashmaps
+    let blue = String::from("Blue");
+    let yellow = String::from("Yellow");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(blue, 10);
+    scores.insert(yellow, 50);
+    //passing blue and yellow pushes ownership of them into our scores hashmap 
+    
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+
+
+    //let mut scores = HashMap::new();
+
+    scores.insert(String::from("Green"), 30);
+    scores.insert(String::from("Green"),45); //overwrite key with new value
+
+    scores.entry(String::from("Green")).or_insert(30);
+    scores.entry(String::from("Green")).or_insert(50);
+
+    for(key, value) in &scores {
+        println!("{} : {}",key,value);
+    } 
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let cnt = map.entry(word).or_insert(0);
+        *cnt += 1
+    }
+    println!("{:#?}",map);
+}   
---
+++ collections/target/debug/deps/collections-f626569614403f91.d
@@ -0,0 +1,5 @@
+/Users/abhishekvenkata/Desktop/learn_rust/collections/target/debug/deps/collections-f626569614403f91: src/main.rs
+
+/Users/abhishekvenkata/Desktop/learn_rust/collections/target/debug/deps/collections-f626569614403f91.d: src/main.rs
+
+src/main.rs:
