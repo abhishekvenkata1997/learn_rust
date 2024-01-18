5101011f73f8c36655f017184e75c239ce9a2ed8
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2024-01-18 15:58:08 -0500

    Creating an art crate to add it to crates.io

---
+++ art/src/main.rs
@@ -0,0 +1,12 @@
+
+use art::PrimaryColor;
+use art::SecondaryColor;
+use art::mix;
+
+fn main() {
+    
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+
+}
